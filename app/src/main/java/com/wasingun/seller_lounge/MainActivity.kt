8754aad31ad7ea6fdaf7cb07123b64b9007cffd4
wasingun <wasingun00@gmail.com>
2023-10-20 16:57:44 +0900

    최초 파일 생성

---
+++ app/src/main/java/com/wasingun/seller_lounge/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.wasingun.seller_lounge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
