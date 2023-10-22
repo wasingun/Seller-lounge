22cea21282fcbda6066566d294d328973d168994
wasingun <wasingun00@gmail.com>
2023-10-23 00:50:26 +0900

    feat: 홈 화면 fragment파일 생성 및 로그인 여부에 따른 화면 분기점 생성, BaseFragment 생성

---
+++ app/src/main/java/com/wasingun/seller_lounge/ui/BaseFragment.kt
@@ -0,0 +1,35 @@
+package com.wasingun.seller_lounge.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<T : ViewDataBinding> : Fragment() {
+    private var _binding: T? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(
+            inflater,
+            getFragmentView(),
+            container,
+            false
+        )
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    abstract fun getFragmentView(): Int
+}
