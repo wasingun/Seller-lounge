f6b6d586f6a14355fdb39a30e34532d91f6a5af3
wasingun <wasingun00@gmail.com>
2023-10-24 16:08:06 +0900

    feature: Bottom navigation의 각 메뉴별 fragment생성

---
+++ app/src/main/java/com/wasingun/seller_lounge/ui/trendcomparison/TrendComparisonFragment.kt
@@ -0,0 +1,12 @@
+package com.wasingun.seller_lounge.ui.trendcomparison
+
+import com.wasingun.seller_lounge.R
+import com.wasingun.seller_lounge.databinding.FragmentTrendComparisonBinding
+import com.wasingun.seller_lounge.ui.BaseFragment
+
+class TrendComparisonFragment: BaseFragment<FragmentTrendComparisonBinding>() {
+
+    override fun getFragmentView(): Int {
+        return R.layout.fragment_trend_comparison
+    }
+}
