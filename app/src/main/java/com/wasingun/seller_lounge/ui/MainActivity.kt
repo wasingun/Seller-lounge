c0c18e63ab99a9fc8a90de5d1628659496c1db79
wasingun <wasingun00@gmail.com>
2023-10-24 15:49:38 +0900

    feature: Bottom navigation 메뉴 추가 및 각 화면 이동 구현

--- app/src/main/java/com/wasingun/seller_lounge/ui/MainActivity.kt
+++ app/src/main/java/com/wasingun/seller_lounge/ui/MainActivity.kt
@@ -2,11 +2,23 @@ package com.wasingun.seller_lounge.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.wasingun.seller_lounge.R
+import com.wasingun.seller_lounge.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController =
+            supportFragmentManager.findFragmentById(binding.containerMain.id)?.findNavController()
+        navController?.let {
+            binding.bnvMain.setupWithNavController(it)
+        }
     }
 }
