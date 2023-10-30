c8bda27f1ecac2e8f08d22389d81f3a200b7305b
wasingun <wasingun00@gmail.com>
2023-10-30 16:37:40 +0900

    refactor: 일부 화면에서 Bottom navigation을 숨기도록 수정

--- app/src/main/java/com/wasingun/seller_lounge/ui/MainActivity.kt
+++ app/src/main/java/com/wasingun/seller_lounge/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.wasingun.seller_lounge.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.wasingun.seller_lounge.R
@@ -14,11 +15,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setBottomNavigation()
+    }
 
+    private fun setBottomNavigation() {
         val navController =
             supportFragmentManager.findFragmentById(binding.containerMain.id)?.findNavController()
         navController?.let {
             binding.bnvMain.setupWithNavController(it)
+        }
+        navController?.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.dest_login || destination.id == R.id.dest_trend_comparison_result) {
+                binding.bnvMain.visibility = View.GONE
+            } else {
+                binding.bnvMain.visibility = View.VISIBLE
+            }
         }
     }
 }
