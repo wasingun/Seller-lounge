fbc1bf33e7edc81718993e8702332053f7d9bcd3
wasingun <wasingun00@gmail.com>
2023-10-30 16:58:23 +0900

    refactor: repository의 반환타입 수정 및 사용자 입력값을 api에 요청할 수 있도록 로직 구현

--- app/src/main/java/com/wasingun/seller_lounge/data/repository/TrendComparisonRepository.kt
+++ app/src/main/java/com/wasingun/seller_lounge/data/repository/TrendComparisonRepository.kt
@@ -1,11 +1,12 @@
 package com.wasingun.seller_lounge.data.repository
 
 import com.wasingun.seller_lounge.data.model.trendcomparison.KeywordRequest
+import com.wasingun.seller_lounge.data.model.trendcomparison.KeywordResponse
 import com.wasingun.seller_lounge.data.source.NaverApiClient
 
 class TrendComparisonRepository(private val naverApiClient: NaverApiClient) {
 
-    suspend fun requestComparisonResult(keywordRequest: KeywordRequest) {
-        naverApiClient.getComparedKeywordData(keywordRequest)
+    suspend fun requestComparisonResult(keywordRequest: KeywordRequest): KeywordResponse {
+        return naverApiClient.getComparedKeywordData(keywordRequest)
     }
 }
