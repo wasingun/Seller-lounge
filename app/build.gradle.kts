b1008ea92771f59f62ac1fa99696f27b2dc6ac1e
wasingun <wasingun00@gmail.com>
2023-10-31 16:00:13 +0900

    chore: Hilt 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     id ("kotlin-parcelize")
     id("androidx.navigation.safeargs")
     id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 val properties = Properties()
@@ -84,8 +85,16 @@ dependencies {
     //MpAndroidChart
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 
+    //hilt
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-compiler:2.44")
+
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+kapt {
+    correctErrorTypes = true
 }
--- app/src/main/java/com/wasingun/seller_lounge/SellerLoungeApplication.kt
+++ app/src/main/java/com/wasingun/seller_lounge/SellerLoungeApplication.kt
@@ -2,7 +2,9 @@ package com.wasingun.seller_lounge
 
 import android.app.Application
 import com.google.firebase.auth.FirebaseAuth
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class SellerLoungeApplication : Application() {
 
     override fun onCreate() {
