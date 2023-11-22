9107db7a9f5ad1cc38eece0ae5ed1bf5023d289e
wasingun <wasingun00@gmail.com>
2023-11-23 04:03:14 +0900

    feat: 홈 화면 유저 정보 업데이트 함수 구현

--- app/src/main/java/com/wasingun/seller_lounge/ui/home/HomeFragment.kt
+++ app/src/main/java/com/wasingun/seller_lounge/ui/home/HomeFragment.kt
@@ -3,30 +3,54 @@ package com.wasingun.seller_lounge.ui.home
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.tabs.TabLayoutMediator
 import com.wasingun.seller_lounge.R
 import com.wasingun.seller_lounge.SellerLoungeApplication
 import com.wasingun.seller_lounge.data.enums.ProductCategory
 import com.wasingun.seller_lounge.databinding.FragmentHomeBinding
+import com.wasingun.seller_lounge.extensions.showTextMessage
 import com.wasingun.seller_lounge.ui.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     private lateinit var adapter: HomeViewPagerAdapter
     private val sharedViewModel by viewModels<HomeSharedViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        adapter = HomeViewPagerAdapter(this)
+        checkLoginState()
+        updateUserInfo()
+        binding.vpHome.adapter = adapter
+        binding.viewModel = sharedViewModel
         binding.btnPost.setOnClickListener {
             moveToPost()
         }
-        adapter = HomeViewPagerAdapter(this)
-        binding.vpHome.adapter = adapter
-        binding.viewModel = sharedViewModel
+        setViewPager()
+        lifecycleScope.launch {
+            sharedViewModel.isError.collect {errorMessage ->
+                if(errorMessage != 0) {
+                    binding.root.showTextMessage(errorMessage)
+                }
+            }
+        }
+    }
 
+    private fun setViewPager() {
         TabLayoutMediator(binding.tlTabMenu, binding.vpHome) { tab, position ->
             tab.text = ProductCategory.values()[position].categoryName
         }.attach()
+    }
+
+    private fun checkLoginState() {
+        if (SellerLoungeApplication.auth.currentUser == null) {
+            val action = HomeFragmentDirections.actionDestHomeToDestLogin()
+            findNavController().navigate(action)
+        }
     }
 
     private fun moveToPost() {
@@ -34,12 +58,8 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
         findNavController().navigate(action)
     }
 
-    override fun onStart() {
-        super.onStart()
-        if (SellerLoungeApplication.auth.currentUser == null) {
-            val action = HomeFragmentDirections.actionDestHomeToDestLogin()
-            findNavController().navigate(action)
-        }
+    private fun updateUserInfo() {
+        sharedViewModel.updateUserInfo()
     }
 
     override fun getFragmentView(): Int {
--- app/src/main/java/com/wasingun/seller_lounge/ui/home/HomeSharedViewModel.kt
+++ app/src/main/java/com/wasingun/seller_lounge/ui/home/HomeSharedViewModel.kt
@@ -1,20 +1,52 @@
 package com.wasingun.seller_lounge.ui.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.wasingun.seller_lounge.R
+import com.wasingun.seller_lounge.SellerLoungeApplication
+import com.wasingun.seller_lounge.data.model.post.UserInfo
+import com.wasingun.seller_lounge.data.repository.GeneralRepository
+import com.wasingun.seller_lounge.network.onError
+import com.wasingun.seller_lounge.network.onException
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeSharedViewModel @Inject constructor(): ViewModel() {
+class HomeSharedViewModel @Inject constructor(private val repository: GeneralRepository) :
+    ViewModel() {
 
+    private val _isError = MutableStateFlow(0)
+    val isError: StateFlow<Int> = _isError
     private val _searchButtonState = MutableStateFlow(false)
-    val searchButtonState:StateFlow<Boolean> = _searchButtonState
+    val searchButtonState: StateFlow<Boolean> = _searchButtonState
+
     val searchKeyword = MutableStateFlow("")
 
     fun searchTitle() {
         _searchButtonState.value = true
         _searchButtonState.value = false
+    }
+
+    fun updateUserInfo() {
+        viewModelScope.launch {
+            val currentUser = SellerLoungeApplication.auth.currentUser
+            val userId = currentUser?.uid ?: ""
+            val userName = currentUser?.displayName ?: ""
+            val userEmail = currentUser?.email ?: ""
+            val userImage = currentUser?.photoUrl.toString() ?: ""
+            val result = repository.userInfoUploadResult(
+                userId, UserInfo(userId, userName, userEmail, userImage)
+            )
+            result.onError { code, message ->
+                _isError.value = R.string.error_user_info_update
+                _isError.value = 0
+            }.onException {
+                _isError.value = R.string.error_api_network
+                _isError.value = 0
+            }
+        }
     }
 }
