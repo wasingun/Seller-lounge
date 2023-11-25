03b9cf572f1eeffccbc7e2005209046a87542dca
wasingun <wasingun00@gmail.com>
2023-11-25 18:02:50 +0900

    feat: 설정화면 레이아웃 구현

--- app/src/main/java/com/wasingun/seller_lounge/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/wasingun/seller_lounge/ui/setting/SettingFragment.kt
@@ -1,10 +1,23 @@
 package com.wasingun.seller_lounge.ui.setting
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.wasingun.seller_lounge.R
+import com.wasingun.seller_lounge.SellerLoungeApplication
 import com.wasingun.seller_lounge.databinding.FragmentSettingBinding
 import com.wasingun.seller_lounge.ui.BaseFragment
 
 class SettingFragment: BaseFragment<FragmentSettingBinding>() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.firebaseUser = SellerLoungeApplication.auth.currentUser
+        binding.tvRecentlyViewedPost.setOnClickListener {
+            val action = SettingFragmentDirections.actionDestSettingToRecentlyViewedPostFragment()
+            findNavController().navigate(action)
+        }
+    }
 
     override fun getFragmentView(): Int {
         return R.layout.fragment_setting
