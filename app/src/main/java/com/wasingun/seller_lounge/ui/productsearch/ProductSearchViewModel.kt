4fe0bea64a574b88aff3779c156920dd38bd0e68
wasingun <wasingun00@gmail.com>
2023-12-01 23:38:56 +0900

    feat: 페이징 아답터 생성 및 상품 검색기능 구현

--- app/src/main/java/com/wasingun/seller_lounge/ui/productsearch/ProductSearchViewModel.kt
+++ app/src/main/java/com/wasingun/seller_lounge/ui/productsearch/ProductSearchViewModel.kt
@@ -1,15 +1,33 @@
 package com.wasingun.seller_lounge.ui.productsearch
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.wasingun.seller_lounge.data.model.localpost.SortSearchType
+import com.wasingun.seller_lounge.data.model.productsearch.ProductInfo
 import com.wasingun.seller_lounge.data.repository.ProductSearchRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class ProductSearchViewModel @Inject constructor(private val repository: ProductSearchRepository) :
     ViewModel() {
+    val searchKeyword = MutableStateFlow<String?>(null)
+    val sortType = MutableStateFlow<String?>(null)
 
-    val searchKeyword = MutableStateFlow<String?>("")
-    val sortType = MutableStateFlow<String?>("")
+    val item = getProductInfoList().cachedIn(viewModelScope)
+
+    fun getProductInfoList(): Flow<PagingData<ProductInfo>> {
+        val inputSearchKeyword = searchKeyword.value
+        val selectedSortType = sortType.value
+
+        val sortTypeList = SortSearchType.values()
+        val sortTypeId = sortTypeList.firstOrNull {
+            it.sortType == selectedSortType
+        }?.id
+        return repository.getProductInfoList(inputSearchKeyword ?: "", sortTypeId ?: "sim", 20)
+    }
 }
